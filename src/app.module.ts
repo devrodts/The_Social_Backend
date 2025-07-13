7976d0739be1ad4bfa8e7924546c35898b9616ed
atous <atous@localhost.localdomain>
2025-07-13 10:51:05 -0300

    feat(app): integrate orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,52 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { join } from "path";
+
+// Import modules
+import { AuthModule } from "./modules/auth/auth.module";
+import { UsersModule } from "./modules/users/users.module";
+import { LikesModule } from "./modules/likes/likes.module";
+import { TweetsModule } from "./modules/tweets/tweets.module";
+
+// Import entities
+import { User } from "./modules/users/entity/user.entity";
+import { Tweet } from "./modules/tweets/entities/tweet.entity";
+import { Like } from "./modules/likes/entities/like.entity";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        entities: [User, Tweet, Like],
+        synchronize: configService.get('NODE_ENV') === 'development',
+        logging: configService.get('NODE_ENV') === 'development',
+      }),
+      inject: [ConfigService],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      playground: true,
+      debug: true,
     }),
+    AuthModule,
+    UsersModule,
+    LikesModule,
+    TweetsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
