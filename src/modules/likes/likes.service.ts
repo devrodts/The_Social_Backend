58e8baf7b0f0fe898e938f98ff8b35a0a6312bc8
atous <atous@localhost.localdomain>
2025-07-13 09:16:25 -0300

    feat: add likes service

---
+++ src/modules/likes/likes.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { CreateLikeUseCase, RemoveLikeUseCase, FindLikesUseCase } from './use-cases';
+import { Like } from './entities/like.entity';
+
+@Injectable()
+export class LikesService {
+  constructor(
+    private readonly createLikeUseCase: CreateLikeUseCase,
+    private readonly removeLikeUseCase: RemoveLikeUseCase,
+    private readonly findLikesUseCase: FindLikesUseCase,
+  ) {}
+
+  async createLike(userId: string, tweetId: string): Promise<Like> {
+    return await this.createLikeUseCase.execute(userId, tweetId);
+  }
+
+  async removeLike(userId: string, tweetId: string): Promise<boolean> {
+    return await this.removeLikeUseCase.execute(userId, tweetId);
+  }
+
+  async findLikesByTweet(tweetId: string, limit: number = 10, offset: number = 0): Promise<Like[]> {
+    return await this.findLikesUseCase.findByTweet(tweetId, limit, offset);
+  }
+
+  async findLikesByUser(userId: string, limit: number = 10, offset: number = 0): Promise<Like[]> {
+    return await this.findLikesUseCase.findByUser(userId, limit, offset);
+  }
+
+  async countLikesByTweet(tweetId: string): Promise<number> {
+    return await this.findLikesUseCase.countByTweet(tweetId);
+  }
+
+  async countLikesByUser(userId: string): Promise<number> {
+    return await this.findLikesUseCase.countByUser(userId);
+  }
+
+  async isLikedByUser(userId: string, tweetId: string): Promise<boolean> {
+    return await this.findLikesUseCase.isLikedByUser(userId, tweetId);
+  }
+}
