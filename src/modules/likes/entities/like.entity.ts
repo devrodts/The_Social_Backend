2beda065ca9c812350cee3b820b99f3f05c1957d
atous <atous@localhost.localdomain>
2025-07-13 08:22:52 -0300

    feat: add like entity

---
+++ src/modules/likes/entities/like.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, JoinColumn, Unique } from 'typeorm';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+
+import { User } from 'src/modules/users/entity/user.entity';
+import { Tweet } from 'src/modules/tweets/entities/tweet.entity';
+
+@ObjectType()
+@Entity('likes')
+@Unique(['userId', 'tweetId'])
+
+export class Like {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field(() => User)
+  @ManyToOne(() => User, user => user.likes, { eager: true })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Field(() => Tweet)
+  @ManyToOne(() => Tweet, tweet => tweet.likes, { eager: true })
+  @JoinColumn({ name: 'tweetId' })
+  tweet: Tweet;
+
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  // Foreign keys for query optimization
+  userId: string;
+  tweetId: string;
+}
